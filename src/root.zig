ddbdfe278415f8a43332dd4deef7e6c91983207c
Chris Birster <christopher.birster@gmail.com>
2026-08-18 16:46:01 -0400

    feat: expose persistent storage

--- src/root.zig
+++ src/root.zig
@@ -5,6 +5,7 @@ pub const card = @import("card.zig");
 pub const deck = @import("deck.zig");
 pub const review = @import("review.zig");
 pub const fsrs = @import("fsrs/root.zig");
+pub const storage = @import("storage/root.zig");
 
 pub const Card = card.Card;
 pub const CardId = card.CardId;
@@ -12,6 +13,7 @@ pub const Deck = deck.Deck;
 pub const DeckId = card.DeckId;
 pub const Review = review.Review;
 pub const ReviewId = review.ReviewId;
+pub const Db = storage.Db;
 
 test {
     std.testing.refAllDecls(@This());
