68d098904b9114a5c82a67b76b7ac98ec0d8dd4a
Chris Birster <christopher.birster@gmail.com>
2026-08-20 21:41:13 -0400

    Include MongoDB parameter-scope acceptance coverage

--- test/mongodb_all.zig
+++ test/mongodb_all.zig
@@ -5,4 +5,5 @@ test {
     _ = @import("mongodb_scheduler_pinning.zig");
     _ = @import("mongodb_parameter_sets.zig");
     _ = @import("mongodb_durable_history.zig");
+    _ = @import("mongodb_parameter_scopes.zig");
 }
