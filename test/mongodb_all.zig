fd0ea49db51893256ff3de2f87f36388695244fd
Chris Birster <christopher.birster@gmail.com>
2026-08-20 21:40:09 -0400

    Include MongoDB durable-history acceptance coverage

--- test/mongodb_all.zig
+++ test/mongodb_all.zig
@@ -4,4 +4,5 @@ test {
     _ = @import("mongodb_integration.zig");
     _ = @import("mongodb_scheduler_pinning.zig");
     _ = @import("mongodb_parameter_sets.zig");
+    _ = @import("mongodb_durable_history.zig");
 }
