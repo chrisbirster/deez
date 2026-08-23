43dc124fdaaaba8b6ed90c9e9a236b87a671bbe2
Chris Birster <christopher.birster@gmail.com>
2026-08-23 14:11:55 -0400

    Expose client-facing card renderer

--- src/root.zig
+++ src/root.zig
@@ -5,6 +5,7 @@ pub const card = @import("card.zig");
 pub const deck = @import("deck.zig");
 pub const content = @import("content.zig");
 pub const interaction = @import("interaction.zig");
+pub const card_render = @import("card_render.zig");
 pub const card_types = @import("card_types.zig");
 pub const portable_content = @import("portable_content.zig");
 pub const render = @import("render.zig");
